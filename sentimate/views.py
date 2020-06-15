8cca3a9a4e6b5360ae51aec5d26c02d1645a580e
Aniket <aniketparsewar@yahoo.co.in>
2020-06-15 12:02:40 -0400

    New Version

--- sentimate/views.py
+++ sentimate/views.py
@@ -31,11 +31,18 @@ def index(request):
     s = tweet(s_tweet)
     for tweets in s:
         #print(tweets.text)
-        # Step 4 Perform Sentiment Analysis on Tweets
-        analysis = TextBlob(tweets.text)
+        # Step 2 Perform Sentiment Analysis on Tweets
+        #analysis = TextBlob(tweets.text)
+
+        if 'retweeted_status' in tweets._json:
+            analysis = TextBlob(tweets._json['retweeted_status']['full_text'])
+            text = 'RT @' + tweets._json['retweeted_status']['user']['screen_name']+':' + tweets._json['retweeted_status']['full_text']
+        else:
+            analysis = TextBlob(tweets.full_text)
+            text = '@' + tweets._json['user']['screen_name']+ ':' + tweets.full_text
         #print if Polarity is not 0
         if analysis.sentiment[0] != 0:
-            tweet_dict[tweets.text] =  analysis.sentiment[0]
+            tweet_dict[text] =  analysis.sentiment[0]
             #print(analysis.sentiment)
             #print("")
 
